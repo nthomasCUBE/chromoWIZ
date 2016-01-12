5a2d8b39a6b7a0ca14e92db1d15a5c311c80c3cb
Thomas Nussbaumer <nussbaumer@giardia.csb.univie.ac.at>
2016-01-12 18:50:05 +0100

    pipeline and config

--- pipeline.sh
+++ pipeline.sh
@@ -1,1 +1,7 @@
+# first step, it needs creating the SQLite-database on the basis of this configuration file
+
 python extract_data.py chromoWIZ.conf
+
+# now, the SQLitge-database allows to generate visualisations
+
+
